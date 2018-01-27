5a1281b1dbfd779129ecd4a0e23b0ab0beffdf0d
Jaysig <jasontsigmon@gmail.com>
2018-01-27 14:44:45 -0600

    Songs now render on SongList

--- Lyrical-GraphQL-master/client/components/SongList.js
+++ Lyrical-GraphQL-master/client/components/SongList.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return (
+        <li>
+          {song.title}
+        </li>
+      )
+    })
+  }
+
   render() {
+
+    if(this.props.data.loading) { return <div>Loading...</div>; }
     return (
       <div>
-        SongList
+        {this.renderSongs()}
       </div>
     )
   }
@@ -19,4 +33,4 @@ const query = gql`
   }
 `;
 
-export default SongList;
+export default graphql(query)(SongList);
