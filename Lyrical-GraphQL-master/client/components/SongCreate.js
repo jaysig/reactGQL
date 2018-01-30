ab02691a75ba160c71ffbad267ef9186eed701a2
Jaysig <jasontsigmon@gmail.com>
2018-01-30 13:45:42 -0600

    Song list now updates when a song is created

--- Lyrical-GraphQL-master/client/components/SongCreate.js
+++ Lyrical-GraphQL-master/client/components/SongCreate.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import { Link, hashHistory } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongCreate extends Component {
   constructor(props) {
@@ -13,10 +14,9 @@ class SongCreate extends Component {
   onSubmit(event){
     event.preventDefault();
     this.props.mutate({
-      variables: {
-        title: this.state.title
-      }
-    }).then(() => hashHistory.push("/"))
+      variables: { title: this.state.title },
+      refetchQueries: [{ query }]
+    }).then(() => hashHistory.push('/'))
   }
 
   render() {
@@ -29,7 +29,7 @@ class SongCreate extends Component {
           <input
             onChange={event => this.setState({ title: event.target.value })}
             value={this.state.title}
-           />
+          />
         </form>
       </div>
     );
