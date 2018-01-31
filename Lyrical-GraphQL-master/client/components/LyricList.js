544cc019a42e0817ca4e754d9d60094a7defaa8a
Jaysig <jasontsigmon@gmail.com>
2018-01-30 18:44:36 -0600

    Likes now increment and are displayed on the frontend

--- Lyrical-GraphQL-master/client/components/LyricList.js
+++ Lyrical-GraphQL-master/client/components/LyricList.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 class LyricList extends Component {
   onLike(id){
-    console.log(id);
+    this.props.mutate({ variables: { id } })
   }
 
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) =>{
+    return this.props.lyrics.map(({ id, content, likes }) =>{
       return (
         <li key={id} className='collection-item'>
           {content}
-          <i className="material-icons" onClick={() => this.onLike(id)}>thumb_up</i>
+          <div className='vote-box'>
+            {likes}
+            <i className="material-icons" onClick={() => this.onLike(id)}>thumb_up</i>
+          </div>
         </li>
       )
     })
@@ -25,4 +30,13 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
