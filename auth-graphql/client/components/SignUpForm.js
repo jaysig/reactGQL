a82451054765328637f5d3d78b8cafb9ea3aae6d
Jaysig <jasontsigmon@gmail.com>
2018-02-07 16:37:35 -0600

    Able to create a user

--- auth-graphql/client/components/SignUpForm.js
+++ auth-graphql/client/components/SignUpForm.js
@@ -4,11 +4,20 @@ import { graphql } from 'react-apollo';
 import mutation from '../mutations/SignUp';
 
 class SignUpForm extends Component {
+  onSubmit({ email, password }) {
+    this.props.mutate({
+      variables: { email, password }
+    }).catch(res => {
+      const errors = res.graphQLErrors.map(error => error.message);
+      this.setState({ errors });
+    });
+  }
+
   render() {
     return (
       <div>
         <h3>Sign Up</h3>
-        <AuthForm />
+        <AuthForm errors={[]} onSubmit={this.onSubmit.bind(this)} />
       </div>
     );
   }
