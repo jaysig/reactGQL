fa8c7f76749f2fc1f760b7855368e37887cf9b05
Jaysig <jasontsigmon@gmail.com>
2018-02-07 16:56:06 -0600

    Added redirect to dashboard for Login and Signup

--- auth-graphql/client/components/SignUpForm.js
+++ auth-graphql/client/components/SignUpForm.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import AuthForm from './AuthForm';
 import { graphql } from 'react-apollo';
 import mutation from '../mutations/SignUp';
+import query from '../queries/CurrentUser';
+import { hashHistory } from 'react-router';
 
 class SignUpForm extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { errors: [] };
+  }
+
+  componentWillUpdate(nextProps) {
+    if (!this.props.data.user && nextProps.data.user) {
+      hashHistory.push('/dashboard');
+    }
+  }
+
   onSubmit({ email, password }) {
     this.props.mutate({
-      variables: { email, password }
+      variables: { email, password },
+      refetchQueries: [{ query }]
     }).catch(res => {
       const errors = res.graphQLErrors.map(error => error.message);
       this.setState({ errors });
@@ -17,10 +32,12 @@ class SignUpForm extends Component {
     return (
       <div>
         <h3>Sign Up</h3>
-        <AuthForm errors={[]} onSubmit={this.onSubmit.bind(this)} />
+        <AuthForm errors={this.state.errors} onSubmit={this.onSubmit.bind(this)} />
       </div>
     );
   }
 }
 
-export default graphql(mutation)(SignUpForm);
+export default graphql(query)(
+  graphql(mutation)(SignUpForm)
+);
