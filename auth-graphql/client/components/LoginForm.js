7cc08e4677d940c2a9e19ef311e53e12eeed02b9
Jaysig <jasontsigmon@gmail.com>
2018-02-07 16:19:30 -0600

    Able to login with the login form

--- auth-graphql/client/components/LoginForm.js
+++ auth-graphql/client/components/LoginForm.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import AuthForm from './AuthForm';
+import mutation from '../mutations/Login';
+import { graphql } from 'react-apollo';
+import query from '../queries/CurrentUser';
 
 class LoginForm extends Component {
+  onSubmit({ email, password }) {
+    this.props.mutate({
+      variables: { email, password },
+      refetchQueries: [{ query }]
+    }).catch(res => {
+      const errors = res.graphQLErrors.map(error => error.message);
+    });
+  }
+
   render() {
     return (
       <div>
         <h3>Login</h3>
-        <AuthForm />
+        <AuthForm onSubmit={this.onSubmit.bind(this)}/>
       </div>
     );
   }
 }
 
-export default LoginForm;
+export default graphql(mutation)(LoginForm);
